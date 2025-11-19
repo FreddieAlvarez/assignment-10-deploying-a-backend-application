16c8affcedbece1b943251b68b11d01283aa6a51
alvarez.freddie@ic.edu <alvarez.freddie@ic.edu>
2025-11-18 19:30:30 -0600

    configured for production environment

--- server.js
+++ server.js
@@ -4,10 +4,13 @@ const jwt = require('jsonwebtoken');
 const { db, User, Task } = require('./database/setup');
 require('dotenv').config();
 
+const cors = require('cors');
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Middleware
+app.use(cors());
 app.use(express.json());
 
 // JWT Authentication Middleware
